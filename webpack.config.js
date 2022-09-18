44b9caec11904a49223395d1d812a13cb6baf8ca
Claudio Catterina <catterina.claudio@gmail.com>
2022-09-18 12:49:27 +0200

    refactor: jekyll and webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/_assets/index.ts",
+  devtool: 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname, "_site/assets"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        // Handle .sass, .scss and .css files
+        test: /\.(sa|sc|c)ss$/,
+
+        // The first loader will be applied after others
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            // Resolves url() and @imports inside CSS
+            loader: "css-loader",
+            options: { importLoaders: 2 },
+          },
+          // Autoprefixer and minifying
+          "postcss-loader",
+          // SASS to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        // Load images
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "images",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "src/_assets/images", to: "images" },
+      ],
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  mode: 'development'
+};
